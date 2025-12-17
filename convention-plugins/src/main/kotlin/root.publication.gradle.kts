a47cb09fd419537be33dbfdb3d43c134c868deb3
YuKongA <70465933+YuKongA@users.noreply.github.com>
2025-12-17 11:19:44 +0800

    library: set next dev version

--- convention-plugins/src/main/kotlin/root.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 val isRelease = project.hasProperty("release")
-val baseVersion = "0.7.2"
+val baseVersion = "0.7.3"
 
 fun gitShort(): String = try {
     val p = ProcessBuilder("git", "rev-parse", "--short", "HEAD").start()
