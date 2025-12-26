7fcbe5275435e835f65e0f87781e8e738e49c039
YuKongA <70465933+YuKongA@users.noreply.github.com>
2025-12-26 17:47:35 +0800

    library: Change miuix-icons namespace
    * [top.yukonga.miuix.kmp:miuix-icons-android:0.7.3-a375015-SNAPSHOT] C:\Users\***\.gradle\caches\9.2.1\transforms\3953e35635b4c707d904334430ff14db\workspace\transformed\miuix-icons-android-0.7.3-a375015-SNAPSHOT\AndroidManifest.xml Warning:
      Namespace 'top.yukonga.miuix.kmp' is used in multiple modules and/or libraries: top.yukonga.miuix.kmp:miuix-icons-android:0.7.3-a375015-SNAPSHOT, top.yukonga.miuix.kmp:miuix-android:0.7.3-a375015-SNAPSHOT. Please ensure that all modules and libraries have a unique namespace. For more information, See https://developer.android.com/studio/build/configure-app-module#set-namespace

--- miuix-icons/build.gradle.kts
+++ miuix-icons/build.gradle.kts
@@ -20,7 +20,7 @@ kotlin {
     android {
         buildToolsVersion = BuildConfig.BUILD_TOOLS_VERSION
         compileSdk = BuildConfig.COMPILE_SDK
-        namespace = BuildConfig.LIBRARY_ID
+        namespace = "${BuildConfig.LIBRARY_ID}.icon"
     }
 
     jvm("desktop")
